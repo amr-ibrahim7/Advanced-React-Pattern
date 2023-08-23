c69c3fdf685ea444088d5e071e22d5404577e75c
amr-ibrahim7 <amrnew100@gmail.com>
2023-08-23 23:40:03 +0300

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from "react";
+
+
+// 1. Create a Context
+const CounterContext = createContext();
+
+
+function Counter({children}) {
+    const [count, setCount] = useState(0);
+    const increase = () =>  setCount((c)=> c + 1)
+    const decrease = () =>  setCount((c)=> c - 1)
+    return (
+       <CounterContext.Provider value={{count,increase,decrease }}>
+        <span>
+            {children}
+        </span>
+       </CounterContext.Provider>
+    )
+  }
+
+
+//   .3 Create child Components
+
+function Count(){
+    const {count} = useContext(CounterContext)
+    return <span>{count}</span>
+}
+
+function Label({children}){
+    return <span>{children}</span>
+}
+function Increase({icon}){
+    const {increase} = useContext(CounterContext)
+    return <button onClick={increase}>{icon}</button>
+
+}
+function Decrease({icon}){
+    const {decrease} = useContext(CounterContext)
+    return <button onClick={decrease}>{icon}</button>
+}
+
+// 4. Add child Components as proeprties to parent components
+Counter.Count =Count;
+Counter.Label =Label;
+Counter.Increase =Increase;
+Counter.Decrease =Decrease;
+
+
+
+  export default Counter;
+  
